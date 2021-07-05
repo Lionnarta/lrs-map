d54cfe546f141f5a73a07e871f43c23892d54c04
lionnarta <lionnarta.savirandy2000@gmail.com>
2021-07-05 15:08:43 +0700

    tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,11 @@ module.exports = {
                 // Margin, Padding, Size
                 'h-screen', 'h-14', 'h-10', 'h-3.5',
                 'z-1000', 
-                'top-4', 'top-0',
-                'left-4', 'left-0',
-                'w-full' ,'w-96', 'w-80', 'w-10', 'w-7', 'w-4', 'w-4/5', 'w-1/2', 'w-1/3','w-1/4',
+                'top-4', 'top-0', 'right-2.5', 'left-4', 'left-0', 'bottom-1/4',
+                'w-full' ,'w-96', 'w-80', 'w-32', 'w-10', 'w-7', 'w-4', 'w-4/5', 'w-1/2', 'w-1/3','w-1/4',
                 'my-0', 'mx-2', 'mx-auto', 'mt-8', 'mt-3', 'mb-8', 'mb-5', 'ml-0.5',
                 'px-2.5', 'py-0.5', 'p-2.5', 'p-2','p-1',
-                'rounded', 'rounded-3xl',
+                'rounded', 'rounded-3xl', 'rounded-lg',
                 'text-5xl', 'font-bold', 'text-left',
                 // Cursor, Font
                 'cursor-pointer',
