9a7198424c9eaa07bc2ed072f3a0cd868f946372
lionnarta <lionnarta.savirandy2000@gmail.com>
2021-07-12 10:22:35 +0700

    a444c09

---
+++ _nuxt/static/1626060137/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flrs-map\u002F_nuxt\u002Fstatic\u002F1626060137",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Flrs-map\u002F",assetsPath:"\u002Flrs-map\u002F_nuxt\u002F",cdnURL:a}}}}(null));
